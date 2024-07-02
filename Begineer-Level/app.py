e1818d4f86ca3e8bd7229a0319fbe4687ee3574f
Mr. Algorithm <148683640+ravixalgorithm@users.noreply.github.com>
2024-07-02 17:06:56 +0530

    python basics done

---
+++ Begineer-Level/app.py
@@ -0,0 +1,46 @@
+"""Start:"""
+# print("Hello world");
+
+"""Drawing a Shape"""
+# print("    /|")
+# print("   / |")
+# print("  /  |")
+# print(" /   |")
+# print("/____|")
+
+"""Variables and DATA TYPES"""
+# character_name = "Ravi";
+# character_age = 20;
+# print("My name is",character_name,"and I am",character_age,"years old.")
+
+"""Working with Strings"""
+phrase = "Giraffe Academy" #str
+print("original:",phrase)
+
+'''to lower case => str.lower()'''
+# print(phrase.lower())
+
+'''to upper case => str.upper()'''
+# print(phrase.upper())
+
+'''check upper and lower case => str.isupper() , str.islower()'''
+# print(phrase.isupper())
+# print(phrase.islower())
+# print(phrase.upper().isupper())
+
+'''length of string => len(str)'''
+# print(len(phrase))
+
+'''print character from str => str[index]'''
+# print(phrase[0])
+
+'''index of character/word => str.index("ch/word")'''
+# print(phrase.index("G"))
+# print(phrase.index("ffe"))
+
+'''replacing a string => str.replace("word to replace","with whom")'''
+# print(phrase.replace("Giraffe","Elephant"))
+
+
+"""Working with Numbers"""
+# time stamp: 39:00
