caff8033c2c9867624e161105dccf5cae6c0751b
Mr. Algorithm <11aravipratapsingh@gmail.com>
2024-09-06 12:54:23 +0530

    update app.py

--- Begineer-Level/app.py
+++ Begineer-Level/app.py
@@ -236,16 +236,31 @@ monthConversion = {
 
 
 """While Loop"""
-i = 1
-while i<=10:
-    print(i)
-    i+=1
+# i = 1
+# while i<=10:
+#     print(i)
+#     i+=1
 
-print("Done with loop")
+# print("Done with loop")
 
 
 """Building a Guessing game"""
+secret_word = "giraffe"
+guess = ""
+guess_count = 0
+guess_limit = 3
+out_of_guesses = False
 
+while guess!= secret_word and not(out_of_guesses):
+    if guess_count < guess_limit:
+        guess = input("Enter your guess: ").lower()
+        guess_count += 1
+    else:
+        out_of_guesses = True
+if out_of_guesses:
+    print("Out of Guesses, You LOSE!")
+else:
+    print("You Win")
 
 
 
---
+++ Begineer-Level/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+
+# while i<=10:
+#     print(i)
+#     i+=1
+
+# print("Done with loop")
